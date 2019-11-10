acfad074b63586b16d14430c1e0df0fb122014b4
JackieBinya <jacquelinebinya@gmail.com>
2019-11-11 00:22:29 +0200

    Set up basic project structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 import Hero from './components/hero/Hero';
+import About from './components/about/About';
+import Projects from './components/projects/Projects';
+import Contact from './components/contact/Contact';
+import Languages from './components/languages/Languages';
 
 const App = () => {
   return (
-    <div className="App">
+    <div>
      <Hero/>
+     <About/>
+     <Languages/>
+     <Projects/>
+     <Contact/>
     </div>
   );
 }
